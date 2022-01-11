120a019c52f67195d96018435b278a462652845b
Abdulkadhar <s.abdulkadhar11@gmail.com>
2022-01-12 00:46:20 +0530

    refine

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:machine_test/screens/login_screen.dart';
 
 void main() {
-  runApp(const LoginScreen());
+  runApp(const MachineTestApp());
+}
+
+class MachineTestApp extends StatelessWidget {
+  const MachineTestApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Login Screen',
+      home: LoginScreen(),
+    );
+  }
 }
