69d54722e4881fdb5bf0b7b31f5f90466828eb37
Abdulkadhar <s.abdulkadhar11@gmail.com>
2022-01-13 05:42:51 +0530

    Addition of restauant get service

---
+++ lib/services.dart/restaurant_service.dart
@@ -0,0 +1,11 @@
+import 'package:machine_test/model/restaurant_response.dart';
+import 'package:machine_test/services.dart/api.dart';
+
+class RestaurantService {
+  static Future<List<RestaurantResponse>> getRestaurantData() async {
+    final jsonData = await ApiService.getData('/v2/5dfccffc310000efc8d2c1ad');
+    return (jsonData as List)
+        .map((e) => RestaurantResponse.fromJson(e))
+        .toList();
+  }
+}
