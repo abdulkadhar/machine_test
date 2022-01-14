1113f702169ba909c213d7a79a10d955f5c8598b
Abdulkadhar <s.abdulkadhar11@gmail.com>
2022-01-14 10:51:22 +0530

    fire base initializer app at main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:machine_test/screens/login_screen.dart';
 
@@ -12,7 +13,46 @@ class MachineTestApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Login Screen',
-      home: LoginScreen(),
+      home: Initializer(),
     );
+  }
+}
+
+class Initializer extends StatelessWidget {
+  const Initializer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: Firebase.initializeApp(),
+        builder: (context, snapshot) {
+          // Check for errors
+          if (snapshot.hasError) {
+            return Expanded(
+              child: Container(
+                color: Colors.white,
+                child: Column(
+                  children: const [
+                    Icon(Icons.info, color: Colors.blue),
+                    Text(
+                      "Something went wrong please restart the application",
+                      style: TextStyle(
+                        color: Colors.blue,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            );
+          }
+
+          // Once complete, show your application
+          if (snapshot.connectionState == ConnectionState.done) {
+            return const LoginScreen();
+          }
+
+          // Otherwise, show something whilst waiting for initialization to complete
+          return const CircularProgressIndicator();
+        });
   }
 }
